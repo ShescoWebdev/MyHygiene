153cedbc8a30cffcd4b71b714b3380a3ad6f4557
Shesco <shedrachafeignatius@gmail.com>
2026-04-04 15:18:27 +0100

    fix: enable service worker registration in main component for PWA support

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-// import { registerSW } from 'virtual:pwa-register'
+import { registerSW } from 'virtual:pwa-register'
 
-// registerSW()
+registerSW()
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
